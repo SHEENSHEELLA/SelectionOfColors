8500e30f2814510d139388e5bd3129558ff0ed5f
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-06 17:04:22 +0300

    Change function generateRandomColor on chroma.js

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const cols = document.querySelectorAll('.col')
 console.log(cols)
 
-function generateRandomColor() {
-  const hexCodes = '0123456789ABCDEF'
-  let color = ''
-  for (let i = 0; i < 6; i++) {
-    color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-  }
-  return '#' + color
-}
+// function generateRandomColor() {
+//   const hexCodes = '0123456789ABCDEF'
+//   let color = ''
+//   for (let i = 0; i < 6; i++) {
+//     color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+//   }
+//   return '#' + color
+// }
 
 function setRandomColors() {
   cols.forEach((col) => {
-    col.style.background = generateRandomColor()
+    const text = col.querySelector('h2')
+    const button = col.querySelector('button')
+    const color = chroma.random()
+
+    text.textContent = color
+    col.style.background = color
+
+    setTextColor(text, color)
+    setTextColor(button, color)
   })
+}
+
+function setTextColor(text, color) {
+  const luminance = chroma(color).luminance()
+  text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
 setRandomColors()
