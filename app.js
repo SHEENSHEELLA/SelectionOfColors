a02ffa6bd96e8a9af925f5c3202f66a495d8e0cc
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-06 18:07:52 +0300

    Add color blocking

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const cols = document.querySelectorAll('.col')
 
 document.addEventListener('keydown', (event) => {
+  event.preventDefault()
   if (event.code.toLowerCase() === 'space') {
     setRandomColors()
+  }
+})
+
+document.addEventListener('click', (event) => {
+  const type = event.target.dataset.type
+  if (type === 'lock') {
+    const node = event.target.tagName.toLowerCase() === 'i' ? event.target : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
   }
 })
 
@@ -17,9 +28,14 @@ document.addEventListener('keydown', (event) => {
 
 function setRandomColors() {
   cols.forEach((col) => {
+    const isLocked = col.querySelector('i').classList.contains('fa-lock')
     const text = col.querySelector('h2')
     const button = col.querySelector('button')
     const color = chroma.random()
+
+    if (isLocked) {
+      return
+    }
 
     text.textContent = color
     col.style.background = color
