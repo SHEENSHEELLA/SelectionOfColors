4d118889df1aa5345203b67543c0b871b08e00c5
Dzmitry Sashcheka <sheensheella@gmail.com>
2023-01-06 21:39:10 +0300

    Store selected colors via a hash

--- app.js
+++ app.js
@@ -32,15 +32,27 @@ function copyToClickBoard(text) {
   return navigator.clipboard.writeText(text)
 }
 
-function setRandomColors() {
-  cols.forEach((col) => {
+function setRandomColors(isInitial) {
+  const colors = isInitial ? getColorsFromHash() : []
+
+  cols.forEach((col, index) => {
     const isLocked = col.querySelector('i').classList.contains('fa-lock')
     const text = col.querySelector('h2')
     const button = col.querySelector('button')
-    const color = chroma.random()
 
     if (isLocked) {
+      colors.push(text.textContent)
       return
+    }
+
+    const color = isInitial
+      ? colors[index]
+        ? colors[index]
+        : chroma.random()
+      : chroma.random()
+
+    if (!isInitial) {
+      colors.push(color)
     }
 
     text.textContent = color
@@ -49,6 +61,8 @@ function setRandomColors() {
     setTextColor(text, color)
     setTextColor(button, color)
   })
+
+  updateColorsHash(colors)
 }
 
 function setTextColor(text, color) {
@@ -56,4 +70,17 @@ function setTextColor(text, color) {
   text.style.color = luminance > 0.5 ? 'black' : 'white'
 }
 
-setRandomColors()
+function updateColorsHash(colors = []) {
+  document.location.hash = colors.map((col) => {
+    return col.toString().substring(1)
+  }).join('-')
+}
+
+function getColorsFromHash() {
+  if (document.location.hash.length > 1) {
+    return document.location.hash.substring(1).split('-').map((color) => '#' + color)
+  }
+  return []
+}
+
+setRandomColors(true)
